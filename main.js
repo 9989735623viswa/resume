84184ff543ba9c7ff4b2085bcdcfae172568c306
viswanath <bviswanath425@gmail.com>
2018-07-31 15:59:42 +0530

    resume

---
+++ main.js
@@ -0,0 +1,30 @@
+// function loadjson(file,callbck)
+// {
+//   varxhr=new xmlhttpRequest();
+//   xhr.override mimetype("application/json");
+//   xhr.open("GET",file,true);
+//   xhr.onready statechang = function()
+// {
+//   if(xhr.redaystate===4&&xhr.state=="200")
+//   callbck(xhr.responsetxt);
+//   }
+// }
+// xhr.send();
+// }
+function loadjson(file){
+  return new Promise((resolve,reject)=>{
+    return fetch(file).then(response=>{
+      if(response.ok){
+        resolve(response.json());
+      }else {
+        reject(new error('error'));
+      }
+    })
+  })
+}
+
+
+var fetchedData=loadjson("data.json");
+fetchedData.then(data=>{
+  console.log(data);
+})
