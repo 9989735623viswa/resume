2d9390ed9dec9497273458369c3f8d001ea25807
viswanath <bviswanath425@gmail.com>
2018-08-01 12:11:02 +0530

    viswa

--- main.js
+++ main.js
@@ -54,6 +54,11 @@ function education(edu){
   child2.appendChild(table);
   var tr="<tr> <td> s.no </td> <td> degree </td> <td> institute </td> <td> data </td> </tr>";
   table.innerHTML=tr;
+  // table.innerHTML=tr;
   table.border="1";
-
+    var tr1="";
+  for (var i = 0; i < edu.length; i++) {
+  tr1+="<tr> <td>"+(i+1)+"</td> <td>"+edu[i].degree+"</td> <td>"+edu[i].institute+"</td> <td>"+edu[i].data+"</td></tr>";
+}
+table.innerHTML=tr+tr1;
 }
